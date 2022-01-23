e3a43b2a3f8781ded2d926503b531e8b0ee7ec63
Lydia Loy <lydia_art@yahoo.com>
2022-01-23 11:40:42 -0600

    added dateFormat.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,4 @@
-const User = require ('./User');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-module.exports = { User };
+const
---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require('exprress').Router();
+const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
+
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
